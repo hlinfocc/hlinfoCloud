567f42665cb9007da149d8390513d0a00c7aca27
hlinfo <kf39@hlinfo.net>
2020-03-28 23:32:13 +0800

    整合公共模块

---
+++ gateway/src/main/java/net/hlinfo/cloud/gateway/controller/IndexController.java
@@ -0,0 +1,27 @@
+package net.hlinfo.cloud.gateway.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/")
+public class IndexController {
+
+	@Value("${spring.application.name}")
+	private String name;
+
+	@GetMapping(value = {"/","/index.html"})
+	public String index() {
+		
+		return "Hello Word ^_^"+name;
+	}
+	@GetMapping("/ok")
+	public String getAreacode() {
+
+		return "ok";
+	}
+
+}
